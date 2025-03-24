28cd7fd9d10471df87e78d203360081432a28078
MHoang287 <Kaimito2000@gmail.com>
2025-03-24 10:24:50 +0700

    Update

--- 2280601038_LeVuMinhHoang/Areas/Admin/Controllers/OrderController.cs
+++ 2280601038_LeVuMinhHoang/Areas/Admin/Controllers/OrderController.cs
@@ -23,6 +23,60 @@ namespace _2280601038_LeVuMinhHoang.Areas.Admin.Controllers
             return View(orders);
         }
 
+        // GET: Admin/Order/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: Admin/Order/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,OrderDate,Total,UserId")] Order order)
+        {
+            if (id != order.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(order);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!OrderExists(order.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
+
         // GET: Admin/Order/Details/5
         public async Task<IActionResult> Details(int? id)
         {
