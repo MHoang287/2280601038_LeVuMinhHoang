45422be83f90a70e299cdd0c67a7aab3dca322e1
Le Vu Minh Hoang <kaimito2000@gmail.com>
2025-04-01 08:10:07 +0700

    Add files via upload

--- 2280601038_LeVuMinhHoang/Models/Product.cs
+++ 2280601038_LeVuMinhHoang/Models/Product.cs
@@ -10,7 +10,6 @@ namespace _2280601038_LeVuMinhHoang.Models
         public int Id { get; set; }
         [Required, StringLength(100)]
         public string Name { get; set; }
-        [Range(0.01, 10000.00)]
         public decimal Price { get; set; }
         public string Description { get; set; }
         public string? ImageUrl { get; set; }
